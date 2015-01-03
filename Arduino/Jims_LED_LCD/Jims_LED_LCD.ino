accdb133cf655db9601759d204b8ac3419c83cfb
Michael <wyrick@umbrasi.com>
2015-01-02 22:09:22 -0500

    Added #defines to use the OLED, LCD, DS18B20 and LEDs Now support for any or all of them can be turned off at compile time

--- Arduino/Jims_LED_LCD/Jims_LED_LCD.ino
+++ Arduino/Jims_LED_LCD/Jims_LED_LCD.ino
@@ -9,26 +9,39 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define VERSION 0.76
+#define VERSION 0.80
 
+#define USE_OLED
+#define USE_LCD
+#define USE_DS18B20
+#define USE_LEDS
+
+#ifdef USE_OLED
 // For OLED
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
+#endif
 
+#ifdef USE_LEDS
 // For LED
 #define PIN 6
 #define LED_COUNT 150
 struct CRGB leds[LED_COUNT];
+#endif
 
+#ifdef USE_LCD
 // For LCD
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6 ,7);
+#endif
 
+#ifdef USE_DS18B20
 // Temperature
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
 #define TEMPERATURE_PRECISION 9
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+#endif
 
 long last_time;
 bool Overflow = false;
@@ -40,10 +53,13 @@ void setup()
   last_time = 0;
   Serial.begin(115200);
 
+#ifdef USE_DS18B20
   // For DS18B20
   sensors.begin();
   sensors.setWaitForConversion(false);
-  
+#endif
+
+#ifdef USE_LCD
  // LCD Text
   lcd.begin (20,4,LCD_5x8DOTS);
   lcd.setBacklightPin(3, POSITIVE);
@@ -54,7 +70,9 @@ void setup()
   lcd.print("  Version:  ");
   lcd.print(VERSION);
   lcd.setCursor(0,0);
+#endif
 
+#ifdef USE_OLED
 //  OLED Graphic
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
   display.setTextColor(WHITE,BLACK);
@@ -73,12 +91,15 @@ void setup()
   display.setCursor(0,35);
   display.print("Mode:");
   display.display();
+#endif
 
+#ifdef USE_LEDS
   // LEDS
   LEDS.addLeds<WS2812B, PIN, GRB>(leds, LED_COUNT);
   LEDS.showColor(CRGB(0, 0, 0));
   LEDS.setBrightness(70); // Limit max current draw to 1A
   LEDS.show(); 
+#endif  
 }
 
 // Send the Ready Signal
@@ -163,8 +184,10 @@ void DoRead() {
       pixelIndex = 0;
       LookForMode = true;
       if (Overflow == false) {
+#ifdef USE_LEDS
         LEDS.show();
         delay(5);
+#endif        
         SignalReady(); 
       } else {
         Overflow = false;
@@ -173,31 +196,38 @@ void DoRead() {
     } else {
       col[colorIndex++] = c;
       if (colorIndex == 3) {
-        leds[pixelIndex] = CRGB(col[0], col[1], col[2]);
         colorIndex = 0;
+#ifdef USE_LEDS      
+        leds[pixelIndex] = CRGB(col[0], col[1], col[2]);
         pixelIndex++;
         if (pixelIndex >= LED_COUNT) {  // All values after read keep updating the last LED
           pixelIndex = LED_COUNT - 1;
         }
+#endif        
       } // index 3
     } // Color Pixel
   } // LookForMode
 }
 
+#ifdef USE_DS18B20
 void UpdateTemp() {
-  float temp = sensors.getTempFByIndex(0);
+  float temp0 = sensors.getTempFByIndex(0);
+  float temp1 = sensors.getTempFByIndex(1);
+#ifdef USE_LCD
   lcd.setCursor(0,2);
   lcd.print("Temp 1:  "); 
-  lcd.print(temp); 
-  temp = sensors.getTempFByIndex(1);
+  lcd.print(temp0); 
   lcd.setCursor(0,3);
   lcd.print("Temp 2:  "); 
-  lcd.print(temp); 
+  lcd.print(temp1); 
+#endif
 }
+#endif
 
 long last_temp = 0;
 // Do Nothing, Everything in Events
 void loop() {
+#ifdef USE_DS18B20
     // Update Temp
   if ((millis() - last_temp) > 1000) {
     UpdateTemp(); 
@@ -205,6 +235,8 @@ void loop() {
     sensors.requestTemperaturesByIndex(1); // Send the command to get temperature
     last_temp = millis();
   }
+#endif
+
   if ((millis() - last_time) > 1000) {
     SignalReady(); 
   }
